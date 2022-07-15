739e82659379c16c69cb66227d29a86f509ac9d9
Roman Khudenko <romanhudenko370@gmail.com>
2022-07-15 11:12:23 +0400

    TestNG initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,31 @@
 package ru.netology.service;
 
-class CashbackHackServiceTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+public class CashbackHackServiceTest {
+
+    @Test
+    public void add100from900() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int actual = service.remain(900);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void add900from100() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 900;
+        int actual = service.remain(100);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void add0from1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(actual, expected);
+    }
 }
