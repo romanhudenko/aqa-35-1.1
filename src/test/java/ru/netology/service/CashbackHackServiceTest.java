43a41a11c77e8340e230fa429418789ed039623f
Roman Khudenko <romanhudenko370@gmail.com>
2022-07-15 11:17:46 +0400

    JUnit4 initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,30 @@
 package ru.netology.service;
 
-class CashbackHackServiceTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class CashbackHackServiceTest {
+    @Test
+    public void add100from900() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int actual = service.remain(900);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void add900from100() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 900;
+        int actual = service.remain(100);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void add0from1000() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+        Assert.assertEquals(expected, actual);
+    }
 }
